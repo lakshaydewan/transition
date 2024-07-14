754ed981a85776d6a1717c2875647831d9894b5f
naman diwan <jaun@namans-MacBook-Air.local>
2024-07-14 22:38:12 +0530

    fixed maybe

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,41 @@ import { animate, easeIn, easeInOut, motion } from 'framer-motion';
 function App() {
 
   return (
-      <div className='flex'>
+      <div className='relative'>
       <motion.div
-      initial={{x:0}}
-      animate={{x:"-50vw"}}
-      transition={{duration: 2, delay:3.5}}
-      exit={{x:"-50vw"}}
-      className='h-screen w-1/2 bg-black relative'
+      initial={{y:0}}
+      animate={{y:"-100vh"}}
+      transition={{duration: 1.7, delay:3.5}}
+      className='absolute top-0 z-50 h-screen w-screen bg-black flex items-center justify-center'
       >
         <motion.div 
         initial={{width: 0, opacity: 1}}
-        animate={{width: 220, opacity: 0}}
+        animate={{width: 330, opacity: 0}}
         transition={{
           width: { duration: 1.5, ease: easeInOut, delay: 1 },
-          opacity: { duration: 1, delay: 2.9 }
+          opacity: { duration: 1.3, delay: 3 }
       }}
-        className='absolute top-1/2 right-0 text-white w-0 h-[1px] bg-white'>
+        className='text-white w-0 h-[1px] bg-white'>
           </motion.div>
-      </motion.div>
-      <motion.div
-      initial={{x:0}}
-      animate={{x:"50vw"}}
-      transition={{duration: 2, delay:3.5}}
-      className='h-screen w-1/2 bg-black'
-      >
+        </motion.div>
         <motion.div 
-        initial={{width: 0, opacity: 1}}
-        animate={{width: 220, opacity: 0}}
-        transition={{
-          width: { duration: 1.5, ease: easeInOut, delay: 1 },
-          opacity: { duration: 1, delay: 2.9 }
-      }}
-        className='absolute top-1/2 left-0 text-white w-0 h-[1px] bg-white'>
-            </motion.div>
-      </motion.div>
+        initial={{y:0}}
+        animate={{y:"-100vh"}}
+        transition={{duration: 1.6, delay:3.9}}
+        className='z-30 w-screen h-screen absolute top-0 bg-[#111111]'>
+        </motion.div>
+        <motion.div 
+        initial={{y:0}}
+        animate={{y:"-100vh"}}
+        transition={{duration: 1.3, delay:4.4}}
+        className='z-20 w-screen h-screen absolute top-0 bg-[#1e1e1e]'>
+        </motion.div>
+        <motion.div 
+        initial={{y:0}}
+        animate={{y:"-100vh"}}
+        transition={{duration: 0.8, delay:4.8}}
+        className='z-10 w-screen h-screen absolute top-0 bg-[#3c3c3c]'>
+        </motion.div>
       </div>
   )
 }
